dcecba1b53a60d8525dee6f8eb7958fd2fae2ce4
EC2 Default User <ec2-user@ip-172-31-89-164.ec2.internal>
2021-06-29 05:47:10 +0000

    Trigger the initial pipeline run.

--- terraform/app-runner/main.tf
+++ terraform/app-runner/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    organization = "" # Enter the Terraform Cloud Organization here
+    organization = "dmstuff" # Enter the Terraform Cloud Organization here
     workspaces {
       name = "app-aws-circleci"
     }
